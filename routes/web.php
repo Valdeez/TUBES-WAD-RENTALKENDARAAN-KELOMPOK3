e1db9f66169c81bccd5faf7b27af62628775aae2
Kevin Muhammad Althaf <kevinmalthaf@gmail.com>
2025-12-29 16:46:02 +0700

    add view motor
    add view motor

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MotorController;
+use App\Models\Motor;
 
 Route::get('/', function () {
     return view('home');
 });
+Route::get('motor', [MotorController::class, 'index'])->name('motor');
+Route::get('/motor/{id}', [MotorController::class, 'show'])->name('motor.detail');
+
+Route::get('/motor/create', [MotorController::class, 'create'])->name('motor.create');
+
+// Route Proses Simpan Motor (Action)
+Route::post('/motor', [MotorController::class, 'store'])->name('motor.store');
